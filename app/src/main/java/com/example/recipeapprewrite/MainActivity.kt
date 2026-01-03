7844cd7e8512dbf2fe0d85f91acca16c20e9f09a
TheDzejkob <axo.lotl.original@gmail.com>
2026-01-03 14:57:40 +0100

    nav bar done

--- app/src/main/java/com/example/recipeapprewrite/MainActivity.kt
+++ app/src/main/java/com/example/recipeapprewrite/MainActivity.kt
@@ -7,9 +7,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,8 +32,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeAppRewriteTheme {
+                // State to keep track of which tab is selected
+                var selectedItem by remember { mutableIntStateOf(0) }
+                val items = listOf("Home", "Add", "Profile")
+                val icons = listOf(Icons.Filled.Home, Icons.Filled.Add, Icons.Filled.Person)
+
                 Scaffold(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    icon = { Icon(icons[index], contentDescription = item) },
+                                    label = { Text(item) },
+                                    selected = selectedItem == index,
+                                    onClick = { selectedItem = index }
+                                )
+                            }
+                        }
+                    }
                 ) { innerPadding ->
                     Box(
                         modifier = Modifier
@@ -30,7 +58,7 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding)
                     ) {
                         Greeting(
-                            name = "Android",
+                            name = items[selectedItem],
                             modifier = Modifier.align(Alignment.Center)
                         )
                     }
